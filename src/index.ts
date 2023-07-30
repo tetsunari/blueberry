7a2e936501cf8530c9232479741401dc130b2906
tetsunari <mtst.tty556@gmail.com>
2023-07-30 17:20:21 +0900

    プリミティブ型

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
-const no: boolean = false;
-const yes: boolean = true;
-console.log(yes, no);
-// true false
+import { createInterface } from 'readline';
 
-const n: null = null;
-const u: undefined = undefined;
-console.log(n, u);
-// null undefined
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+// rl.question('文字列を入力してください:', (line) => {
+//     console.log(`${line}が入力されました`);
+//     rl.close();
+// });
+
+// rl.question('数値を入力してください:', (line) => {
+//     console.log(line + 1000);
+//     rl.close();
+// });
+
+// rl.question('数値を入力してください:', (line) => {
+    // const result = line + 1000;
+    // console.log(result);
+    // rl.close();
+// });
+
+// rl.question('数値を入力してください:', (line) => {
+    // const result = line + 1000;
+    // console.log(result);
+    // rl.close();
+// })
+
+rl.question('数値を入力してください:', (line) => {
+    const num = Number(line);
+    console.log(num + 1000);
+    rl.close();
+});
+
+
+// 文字列を入力してください:foobar
+// foobarが入力されました
+// 数値を入力してください:1234
+// 12341000
+// 数値を入力してください:1234
+// 12341000
+// 数値を入力してください:1234
+// 2234
+// 数値を入力してください:foobar
+// NaN
