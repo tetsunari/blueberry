bea968e9193bc6d504a9c5c012d8769bab3dd727
tetsunari <mtst.tty556@gmail.com>
2023-07-30 15:18:56 +0900

    type annotation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const あいう = 123;
-const 技術評論 = あいう + 876;
-console.log(技術評論);
+const greeting: string = "Hello, ";
+const target: string = "world!";
+console.log(greeting + target);
+
+
+const greeting1: string = "Hello, ";
+const target1: string = 123;
+// コンパイルエラーになる
+console.log(greeting1 + target1)
