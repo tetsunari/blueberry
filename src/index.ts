10617f071268c632c7293fadcef33c5951e02f2c
tetsunari <mtst.tty556@gmail.com>
2023-07-30 17:37:32 +0900

    プリミティブ型

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,60 @@
-import { createInterface } from 'readline';
+const num1 = Number(true);
+console.log(num1);
+// 1
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const num2 = Number(false);
+console.log(num2);
+// 0
 
-// rl.question('文字列を入力してください:', (line) => {
-//     console.log(`${line}が入力されました`);
-//     rl.close();
-// });
+const num3 = Number(null);
+console.log(num3);
+// 0
 
-// rl.question('数値を入力してください:', (line) => {
-//     console.log(line + 1000);
-//     rl.close();
-// });
-
-// rl.question('数値を入力してください:', (line) => {
-    // const result = line + 1000;
-    // console.log(result);
-    // rl.close();
-// });
-
-// rl.question('数値を入力してください:', (line) => {
-    // const result = line + 1000;
-    // console.log(result);
-    // rl.close();
-// })
-
-rl.question('数値を入力してください:', (line) => {
-    const num = Number(line);
-    console.log(num + 1000);
-    rl.close();
-});
-
-
-// 文字列を入力してください:foobar
-// foobarが入力されました
-// 数値を入力してください:1234
-// 12341000
-// 数値を入力してください:1234
-// 12341000
-// 数値を入力してください:1234
-// 2234
-// 数値を入力してください:foobar
+const num4 = Number(undefined);
+console.log(num4);
 // NaN
+
+const bigint1 = BigInt("1234");
+console.log(bigint1);
+// 1234n
+
+const bigint2 = BigInt(500);
+console.log(bigint2);
+// 500n
+
+const bigint3 = BigInt(true);
+console.log(bigint3);
+// 1n
+
+// const bigint = BigInt("foobar");
+// ランタイムエラー
+
+const str1 = String(1234.5);
+console.log(str1);
+// "1234.5"
+
+const str2 = String(true);
+console.log(str2);
+// "true"
+
+const str3 = String(null);
+const str4 = String(undefined);
+console.log(str3, str4);
+// "null undefined"
+
+console.log(Boolean(123));
+// true
+console.log(Boolean(0));
+// false
+console.log(Boolean(1n));
+// true
+console.log(Boolean(0n));
+// false
+console.log(Boolean(""));
+// false
+console.log(Boolean("foobar"));
+// true
+console.log(Boolean(null));
+// false
+console.log(Boolean(undefined));
+// false
