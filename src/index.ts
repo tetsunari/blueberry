ddfe6bf7b86421b820b08e39efa54b7516acd83e
tetsunari <mtst.tty556@gmail.com>
2023-07-30 15:31:43 +0900

    number

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
-let greeting: string = "Hello, ";
-let target: string = "world!";
-console.log(greeting + target);
-
-const greeting1 = "Hello, ";
-// コンパイルエラー
-greeting1 = greeting1 + "world!";
-
-let greeting2, target2;
-greeting2 = "Hello, ";
-target2 = "world!";
-console.log(greeting2 + target2);
-
-
-let greeting3: string, target3: string;
-greeting3 = "Hello, ";
-target3 = "world!";
-console.log(greeting3 + target3);
-
-let greeting4: string, target4: string;
-greeting4 = "Hello, ";
-// target4が定義されていないのでエラー
-console.log(greeting4 + target4);
+const width1 = 5;
+const width2 = 8;
+const height: number = 3;
+const area = (width1 + width2) * height / 2;
+console.log(area);
