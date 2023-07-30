70e80293c0e38983049e3a32f6095d23a23271c0
tetsunari <mtst.tty556@gmail.com>
2023-07-30 16:19:11 +0900

    文字列のエスケープシーケンス

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-const str1: string = "Hello";
-const str2: string = 'world!';
-console.log(str1 + ", " + str2);
-// Hello, world!
+console.log("Hello \\word!/");
+// Hello \word!/
 
-const message: string = `Hello
-world!`;
-console.log(message);
-// Hello
-// world!
-
-const str3: string = "Hello";
-const str4: string = "world";
-console.log(`${str3}, ${str4}`);
-// Hello, world
-
-console.log(`123 + 456 = ${123 + 456}`);
-// 123 + 456 = 579
+console.log("Hello \u{796d} world!");
+// Hello 祭 world!
