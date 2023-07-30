2df1b3d916e9dc5120af2ab35c42495b33c196d8
tetsunari <mtst.tty556@gmail.com>
2023-07-30 20:32:11 +0900

    真偽値の演算

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
-const n: null = null;
-const u: undefined = undefined;
+import { createInterface } from 'readline';
 
-console.log(n === null);
-// true
-console.log(n === undefined);
-// false
-console.log(u === null);
-// false
-console.log(u === undefined);
-// true
-console.log(n == null);
-// true
-console.log(u == null);
-// true
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('数値を入力してください:', (line) => {
+    const num = Number(line);
+
+    if (0 <= num && num < 100) {
+        console.log(`${num}は0以上100未満です`);
+    } else {
+        console.log(`${num}は0以下100未満ではありません`);
+    }
+    if (!Number.isNaN(num)) {
+        console.log(num,"はNANではありません")
+    }
+    rl.close();
+})
+
+// 数値を入力してください:10
+// 10は0以上100未満です
+// 10 はNANではありません
+// root@cb390b530e8a:/app# node dist/index.js
+// 数値を入力してください:100
+// 100は0以下100未満ではありません
+// 100 はNANではありません
+// root@cb390b530e8a:/app# node dist/index.js
+// 数値を入力してください:test
+// NaNは0以下100未満ではありません
+// !Number.isNaNは　NaNでない時のみ処理を行いたい場合
