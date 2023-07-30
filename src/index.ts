b309a4a8b485ee8a22439cc7fc4887137e162a96
tetsunari <mtst.tty556@gmail.com>
2023-07-30 16:04:31 +0900

    bigint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-const big = 1e8;
-const small = 4e-5;
-console.log(big, small);
-// 100000000 0.00004
+const bignum: bigint = (123n + 456n) * 2n;
+console.log(bignum);
+// 1158n
 
-const million = 1_000_000;
-console.log(million);
-// 1000000
+const result = 5n / 2n;
+console.log(result);
+// 2n
