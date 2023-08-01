d95ebc5774c9a40bbe06654ad0ed88906ed9722a
tetsunari <mtst.tty556@gmail.com>
2023-08-01 23:09:26 +0900

    代入演算子

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,26 @@ const rl = createInterface({
 });
 
 rl.question('名前を入力してください:', (name) => {
-    const displayName = name || "名無し";
-    const displayIsInput = Boolean(name) && "入力済";
-    console.log(`こんにちは、${displayName}さん`);
-    console.log(`${displayIsInput}`);
+    if (name === '') {
+        name = '名無し'
+    }
+    console.log(`こんにちは${name}さん`);
     rl.close();
 })
 
-// 名前を入力してください:
-// こんにちは、名無しさん
-// false
-// root@cb390b530e8a:/app# node dist/index.js
-// 名前を入力してください:test
-// こんにちは、testさん
-// 入力済
-
-const secret = process.env.SECRET ?? "default";
-console.log(`secretは${secret}です`);
-// ??は nullと undefinedのみ判定する
-// SECRET=foo node dist/index.ts
-//　を実行すると secretはfooです
-// 何も指定しないと secretはdefaultです
+let num = 0;
+num += 100;
+console.log(num);
+// 100
+num *= 4;
+console.log(num);
+// 400
+num -= 200;
+console.log(num);
+// 200
+num /= 2;
+console.log(num);
+// 100
+num **= 0.5;
+console.log(num);
+// 10
