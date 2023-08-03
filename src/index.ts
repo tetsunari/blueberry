6452061cef9afc3b49ebb292e73722c65013ddd9
tetsunari <mtst.tty556@gmail.com>
2023-08-03 23:28:51 +0900

    fizzbuzz

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-let sum = 0;
-for (let i: number = 1; i <= 20; i++) {
-    sum += i;
+for (let i: number = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        if (i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else {
+            console.log('Fizz');
+        }
+    } else {
+        if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
 }
-console.log(sum);
