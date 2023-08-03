428ac4ee49f5c7bcc99e5a6e802b32fede3b4567
tetsunari <mtst.tty556@gmail.com>
2023-08-03 23:17:29 +0900

    for文

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-import { createInterface } from 'readline';
-
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('名前を入力してください:', (name) => {
-    if (name === '') {
-        name = '名無し'
-    }
-    console.log(`こんにちは${name}さん`);
-    rl.close();
-})
-
-let num = 0;
-num += 100;
-console.log(num);
-// 100
-num *= 4;
-console.log(num);
-// 400
-num -= 200;
-console.log(num);
-// 200
-num /= 2;
-console.log(num);
-// 100
-num **= 0.5;
-console.log(num);
-// 10
+let sum = 0;
+for (let i: number = 1; i <= 20; i++) {
+    sum += i;
+}
+console.log(sum);
