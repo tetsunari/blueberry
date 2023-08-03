9f81de7ed14cff1673df8ce05690990ee4901e49
tetsunari <mtst.tty556@gmail.com>
2023-08-03 23:43:05 +0900

    object

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-let log: string = '';
-for (let i: number = 1; i <= 100; i++) {
-    if (i > 1) {
-        log += ' ';
-    }
-    if (i % 3 === 0) {
-        if (i % 5 === 0) {
-            log += 'FizzBuzz';
-        } else {
-            log += 'Fizz';
-        }
-    } else {
-        if (i % 5 === 0) {
-            log += 'Buzz';
-        } else {
-            log += String(i);
-        }
-    }
-    console.log(log);
-}
+const obj = {
+    foo: 123,
+    bar: 'Hello, world'
+};
+
+console.log(obj.foo);
+console.log(obj.bar);
