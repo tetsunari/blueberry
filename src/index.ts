09ca7b07bc12ee44c8e3603b06c4dd3d7c409855
tetsunari <mtst.tty556@gmail.com>
2023-08-06 21:23:15 +0900

    プロパティへの代入

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-const obj = {
-    "foo": 123,
-    "foo bar": -500,
-    "↑↓↑↓": ""
+const user = {
+    name: "uhyo",
+    age: 25,
 };
-console.log(obj.foo); // 123
-console.log(obj["foo bar"]); // -500
 
-const obj1 = {
-    1: "one",
-    2.05: "two point o five",
-};
-console.log(obj1["1"]); // one
-console.log(obj1["2.05"]); // two point o five
-
-const propName = "foo";
-const obj2 = {
-    [propName]: 123
-};
-console.log(obj2.foo); // 123
+user.age = 26;
+console.log(user.age); // 26
