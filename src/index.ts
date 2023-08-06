757b19e75706a8ebcf9de1ff85730db438c8a432
tetsunari <mtst.tty556@gmail.com>
2023-08-06 21:44:35 +0900

    スプレッド構文

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,51 @@
-import {createInterface} from 'readline';
+const obj1 = {
+    bar: 456,
+    baz: 789,
+};
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const obj2 = {
+    foo: 123,
+    ...obj1
+};
+console.log(obj2); // { foo: 123, bar: 456, baz: 789 }
 
-const messages = {
-    good: '0以上の数値が入力されました！',
-    bad: '負の数値を入力しないでください！'
-}
 
-rl.question('数値を入力してください：', (line) => {
-    const num = Number(line);
-    console.log(messages[num >= 0 ? "good" : "bad"]);
-    rl.close();
-});
+const obj3 = {
+    foo: 123,
+    bar: 456,
+    baz: 789,
+};
+
+const obj4 = {
+    ...obj3,
+    foo: -999,
+};
+console.log(obj4); // { foo: -999, bar: 456, baz: 789 }
+
+
+// const obj5 = {
+    // foo: 123,
+    // bar: 456,
+    // baz: 789,
+// };
+//
+// const obj6 = {
+    // foo: -999,
+    // ...obj5
+// };
+// 'foo' is specified more than once, so this usage will be overwritten.
+
+
+const obj7 = {
+    foo: 123,
+    bar: 456,
+};
+const obj8 = {
+    bar: -999,
+    baz: -9999,
+};
+const obj9 = {
+    ...obj7,
+    ...obj8
+};
+console.log(obj9); // { foo: 123, bar: -999, baz: -9999 }
