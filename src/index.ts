87b81c556eaeba788bde221b9beffc0a9a90ca70
tetsunari <mtst.tty556@gmail.com>
2023-08-06 22:21:31 +0900

    objectの値の変異

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,22 @@
-const obj1 = {
-    bar: 456,
-    baz: 789,
-};
+const foo = { num: 1234 };
+const bar = foo;
+console.log(bar.num); // 1234
+bar.num = 0;
+console.log(foo.num); // 0
 
-const obj2 = {
-    foo: 123,
-    ...obj1
-};
-console.log(obj2); // { foo: 123, bar: 456, baz: 789 }
+const foo1 = { num: 1234 };
+const bar1 = { ...foo1 };
+console.log(bar1.num); // 1234
+bar1.num = 0;
+console.log(foo1.num); // 1234
 
+const foo2 = { obj: { num: 1234 } };
+const bar2 = { ...foo2 };
+bar2.obj.num = 0;
+console.log(foo2.obj.num); //0
 
-const obj3 = {
-    foo: 123,
-    bar: 456,
-    baz: 789,
-};
-
-const obj4 = {
-    ...obj3,
-    foo: -999,
-};
-console.log(obj4); // { foo: -999, bar: 456, baz: 789 }
-
-
-// const obj5 = {
-    // foo: 123,
-    // bar: 456,
-    // baz: 789,
-// };
-//
-// const obj6 = {
-    // foo: -999,
-    // ...obj5
-// };
-// 'foo' is specified more than once, so this usage will be overwritten.
-
-
-const obj7 = {
-    foo: 123,
-    bar: 456,
-};
-const obj8 = {
-    bar: -999,
-    baz: -9999,
-};
-const obj9 = {
-    ...obj7,
-    ...obj8
-};
-console.log(obj9); // { foo: 123, bar: -999, baz: -9999 }
+const foo3 = { num: 1234 };
+const bar3 = foo3;
+const baz3 = { num: 1234 };
+console.log(foo3 === bar3); // true
+console.log(foo3 === baz3); // false
