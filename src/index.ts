778a20cdfb298330058d14dc82dad3fbc51bd815
tetsunari <mtst.tty556@gmail.com>
2023-08-06 21:13:32 +0900

    object 基本構文

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 const obj = {
-    foo: 123,
-    bar: 'Hello, world'
+    foo: 555,
+    bar: '文字列',
 };
 
 console.log(obj.foo);
 console.log(obj.bar);
+
+const user = {
+    name: input ? input : '名無し',
+    age: 20,
+}
+
+const name1 = input ? input : '名無し';
+const user1 = {
+    name: name1,
+    age: 20,
+}
+
+const name = input ? input : '名無し';
+const user2 = {
+    name,
+    age: 20,
+}
