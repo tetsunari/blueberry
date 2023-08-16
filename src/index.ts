981a3ab8aec1e994fd3252b9a8ad4f5be4470219
tetsunari <mtst.tty556@gmail.com>
2023-08-17 00:04:58 +0900

    インデックスシグネチャ

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-interface FooBarObj {
-    foo: number;
-    bar: string;
+type PriceData = {
+    [key: string]: number;
 }
-const obj: FooBarObj = {
-    foo: 0,
-    bar: "string",
+const data: PriceData = {
+    apple: 220,
+    coffee: 120,
+    bento: 500,
 };
+data.chicken = 250;
+// data.弁当 = "foo"; // error
+
+type MyObj = { [key: string]: number };
+const obj: MyObj = { foo: 123 };
+const num: number = obj.bar;
+console.log(num); // undefined
+
+const propName: string = "foo";
+const obj1 = {
+    [propName]: 123
+};
+console.log(obj1.foo); // 123
