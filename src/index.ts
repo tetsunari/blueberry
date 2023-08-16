004c1157a295d15aec8f958f36c2ab610d3e90f5
tetsunari <mtst.tty556@gmail.com>
2023-08-16 23:31:47 +0900

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-type FooBarObj = {
+interface FooBarObj {
     foo: number;
     bar: string;
-};
+}
 const obj: FooBarObj = {
-    foo: 123,
-    bar: "Hello, world",
+    foo: 0,
+    bar: "string",
 };
-
-const obj1: FooBarObj1 = {
-    foo: 123,
-    bar: "Hello, world",
-};
-type FooBarObj1 = {
-    foo: number;
-    bar: string;
-};
-
-type UserId = string;
-const id: UserId = "uhyo";
-
-type FooBar = { foo: number };
-type MyObj = FooBar;
-const obj2: MyObj = { foo: 0 };
