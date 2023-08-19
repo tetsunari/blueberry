7cbc1753bb64dddecd29a34e06a61afddd653a5a
tetsunari <mtst.tty556@gmail.com>
2023-08-20 01:04:50 +0900

    typeof

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@ const num: number = 0;
 type T = typeof num;
 const foo: T = 123;
 
+const obj = {
+    foo: 123,
+    bar: "hi"
+};
+type O = typeof obj;
+const obj2: O = {
+    foo: -50,
+    bar: ""
+};
+
+const res: typeof foo = typeof bar;
