d3b1a3400da64f964104eb56b56f8776e5695a92
tetsunari <mtst.tty556@gmail.com>
2023-08-20 01:14:23 +0900

    部分型

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-const num: number = 0;
-type T = typeof num;
-const foo: T = 123;
-
-const obj = {
-    foo: 123,
-    bar: "hi"
+type FooBar = {
+    foo: string;
+    bar: number;
+}
+type FooBarBaz = {
+    foo: string;
+    bar: number;
+    baz: boolean;
+}
+const obj: FooBarBaz = {
+    foo: "hi",
+    bar: 1,
+    baz: false,
 };
-type O = typeof obj;
-const obj2: O = {
-    foo: -50,
-    bar: ""
-};
-
-const res: typeof foo = typeof bar;
+const obj2: FooBar = obj;
