52de18af7f14c9e927c1979c53f04b9f472f5947
tetsunari <mtst.tty556@gmail.com>
2023-08-21 23:26:30 +0900

    部分型関係による型引数の制約

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-type User<T> = {
+type HasName = {
     name: string;
-    child: T;
 };
-type Family <Parent, Child> = {
+type Family<Parent extends HasName, Child extends HasName> = {
     mother: Parent;
     father: Parent;
     child: Child;
 };
+// type T = Family<number, string>; // error
 
-const obj: Family<number, string> = {
-    mother: 0,
-    father: 100,
-    child: "1000"
+type Animal = {
+    name: string;
 };
+type Human = {
+    name: string;
+    age: number;
+};
+type T1 = Family<Animal, Human>;
+
+type Family1<Parent extends HasName, Child extends Parent> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+};
+type S = Family1<Animal, Human>;
+// type T2 = Family1<Human, Animal>; // error
