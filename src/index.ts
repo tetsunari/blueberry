99b9d1e86af4d94a0ce42256b61d294271ea25b7
tetsunari <mtst.tty556@gmail.com>
2023-08-21 23:36:33 +0900

    オプショナルな型

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-type HasName = {
-    name: string;
-};
-type Family<Parent extends HasName, Child extends HasName> = {
-    mother: Parent;
-    father: Parent;
-    child: Child;
-};
-// type T = Family<number, string>; // error
-
 type Animal = {
     name: string;
 };
-type Human = {
-    name: string;
-    age: number;
-};
-type T1 = Family<Animal, Human>;
-
-type Family1<Parent extends HasName, Child extends Parent> = {
+type Family<Parent = Animal, Child = Animal> = {
     mother: Parent;
     father: Parent;
     child: Child;
 };
-type S = Family1<Animal, Human>;
-// type T2 = Family1<Human, Animal>; // error
+type S = Family<string, string>
+type T = Family;
+type U = Family<string>;
+
+type Family1<Parent, Child = Animal> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+};
+
+type HasName = {
+    name: string;
+};
+type Family2<Parent extends HasName, Child extends HasName = Animal> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+};
