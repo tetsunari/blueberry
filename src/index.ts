62f6d7eea758026393c6ceb390a1f00cf5b9d26d
tetsunari <mtst.tty556@gmail.com>
2023-08-23 23:51:29 +0900

    配列

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-type Animal = {
-    name: string;
-};
-type Family<Parent = Animal, Child = Animal> = {
-    mother: Parent;
-    father: Parent;
-    child: Child;
-};
-type S = Family<string, string>
-type T = Family;
-type U = Family<string>;
-
-type Family1<Parent, Child = Animal> = {
-    mother: Parent;
-    father: Parent;
-    child: Child;
-};
-
-type HasName = {
-    name: string;
-};
-type Family2<Parent extends HasName, Child extends HasName = Animal> = {
-    mother: Parent;
-    father: Parent;
-    child: Child;
-};
+const arr = [0, 123, -456 * 100];
+console.log(arr);
+const arr2 = [100, "文字列", false];
+console.log(arr2);
+const arr3 = [4, 5, 6];
+const arr4 = [1, 2, 3, ...arr3];
+console.log(arr4);
