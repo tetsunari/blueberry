eceb3e81bc63801ab4bc438790ab2ce4b54c5f36
tetsunari <mtst.tty556@gmail.com>
2023-08-29 00:21:42 +0900

    配列型の記法

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-const arr = [0, 123, -456 * 100];
-console.log(arr[0]); // 0
-console.log(arr[1]); // 123
+const arr: number[] = [1, 10, 100];
+// const arr2: string[] = [123, -456]; // error
 
-const arr1 = [0, 123, -456 * 100];
-console.log(arr1); // [ 0, 123, -45600 ]
-arr1[1] = 5400;
-console.log(arr1); // [ 0, 5400, -45600 ]
+const arr3: boolean[] = [false, true];
+const arr4: Array<{
+    name: string
+}> = [
+    { name: "山田さん" },
+    { name: "田中さん" },
+    { name: "鈴木さん" },
+];
 
-const arr2 = [0, 123, -456 * 100];
-arr2[1] = 5400;
-// arr2 = [1, 3, 45, 6]; // error
+const arr5 = [1, 10, 100];
+const arr6 = [100, "文字列", false];
