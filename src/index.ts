bc3b71e7c8f7c38bfd250b50a520983afc7dd829
tetsunari <mtst.tty556@gmail.com>
2023-08-29 00:14:46 +0900

    配列にアクセス

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 const arr = [0, 123, -456 * 100];
-console.log(arr);
-const arr2 = [100, "文字列", false];
-console.log(arr2);
-const arr3 = [4, 5, 6];
-const arr4 = [1, 2, 3, ...arr3];
-console.log(arr4);
+console.log(arr[0]); // 0
+console.log(arr[1]); // 123
+
+const arr1 = [0, 123, -456 * 100];
+console.log(arr1); // [ 0, 123, -45600 ]
+arr1[1] = 5400;
+console.log(arr1); // [ 0, 5400, -45600 ]
+
+const arr2 = [0, 123, -456 * 100];
+arr2[1] = 5400;
+// arr2 = [1, 3, 45, 6]; // error
