b65af7f4db7df11891cd5885a45924b8afe4ccd9
tetsunari <mtst.tty556@gmail.com>
2023-08-29 23:06:06 +0900

    readonly

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-const arr: number[] = [1, 10, 100];
-// const arr2: string[] = [123, -456]; // error
-
-const arr3: boolean[] = [false, true];
-const arr4: Array<{
-    name: string
-}> = [
-    { name: "山田さん" },
-    { name: "田中さん" },
-    { name: "鈴木さん" },
-];
-
-const arr5 = [1, 10, 100];
-const arr6 = [100, "文字列", false];
+const arr: readonly number[] = [1, 10, 100];
+// arr[1] = -500; // error
