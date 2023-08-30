f6cd951600ed13e41cf9d79daeefa86990dbccc8
tetsunari <mtst.tty556@gmail.com>
2023-08-30 23:36:55 +0900

    分割代入

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-let tuple: [string, number] = ["foo", 0];
-tuple = ["aiueo", -552];
-const str = tuple[0];
-const num = tuple[1];
+const { foo, bar } = obj;
+const {
+    foo,
+    bar: barVar,
+    "foo bar": foobar
+} = obj1;
 
-type User = [name: string, age: number];
-const uhyo: User = ["uhyo", 26];
-console.log(uhyo[1]);
-// console.log(uhyo.name); // error
+const obj2 = {
+    str: "hello, world",
+    num: 1234
+};
+const {
+    foo
+} = obj2; // error
