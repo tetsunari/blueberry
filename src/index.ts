46b49d8c47baf97ec5aca6041bd0a002bcb23fbb
tetsunari <mtst.tty556@gmail.com>
2023-09-05 00:02:56 +0900

    三項演算子

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ type obj = { foo?: number };
 const obj1: obj = {};
 const obj2: obj = { foo: -1234 };
 const { foo = 500 } = obj1;
+// const foo = obj.foo !== undefined ? obj1.foo : 500; // 上記と同じ意味
 console.log(foo);
 const { foo: bar = 500 } = obj2;
 console.log(bar);
