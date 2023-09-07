cfd6ca47da57e11b14ebbafc14db44e3c4e70557
tetsunari <mtst.tty556@gmail.com>
2023-09-07 23:49:53 +0900

    分割代入のデフォルト値

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-type obj = { foo?: number };
-const obj1: obj = {};
-const obj2: obj = { foo: -1234 };
-const { foo = 500 } = obj1;
-// const foo = obj.foo !== undefined ? obj1.foo : 500; // 上記と同じ意味
-console.log(foo);
-const { foo: bar = 500 } = obj2;
-console.log(bar);
+const obj = { foo: null };
+const { foo = 123 } = obj;
+console.log(foo); // デフォルト値はundefinedのみに対して適応される
+
+type Obj = { bar?: number };
+const obj1: Obj = {};
+const { bar = 500 } = obj1;
+console.log(bar); // 500
+
+type NestedObj = {
+    obj?: {
+        foobar: number
+    }
+};
+const nested1: NestedObj = {
+    obj: { foobar: 123 }
+};
+const nested2: NestedObj = {};
+const { obj: { foobar: foo1 } = { foobar: 500 } } = nested1;
+const { obj: { foobar: foo2 } = { foobar: 500} } = nested2;
+console.log(foo1); // 123
+console.log(foo2); // 500
