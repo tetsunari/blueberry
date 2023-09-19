5684324017705cf224d416f37e3ca42bafb99597
tetsunari <mtst.tty556@gmail.com>
2023-09-19 23:21:59 +0900

    データの処理

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
-const str = "Hello, world";
-console.log(str.length); // 12
+type User = {
+    name: string;
+    age: number;
+    premiumUser: boolean;
+}
 
-type HasLength = { length: number };
-const obj:HasLength = "foobar";
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary SubmitEvent,14,1
+`;
 
-let val: {} = 123;
-val = "foobar";
-val = { num: 1234 };
-// val = null; // error
-// val = undefined; // error
+const users: User[] = [];
+const split = data.split("\n");
+for (const v of split) {
+    if (v === '') continue;
+
+    const [name, ageString, premiumUserString] = v.split(",");
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === "1";
+
+    users.push({
+        name,
+        age,
+        premiumUser
+    })
+}
+
+for (const user of users) {
+    if (user.premiumUser) {
+        console.log(`${user.name} (${user.age}) is premium user.`);
+    } else {
+        console.log(`${user.name} (${user.age} is not premium user.)`)
+    }
+}
+
+/**
+uhyo (26) is premium user.
+John Smith (17 is not premium user.)
+Mary SubmitEvent (14) is premium user.
+ */
