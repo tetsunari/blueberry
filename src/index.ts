d1857552750f4905d0635f5a2b9d2cd7ebdc8a3a
tetsunari <mtst.tty556@gmail.com>
2023-09-19 23:05:04 +0900

    プリミティブなプロパティ

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-console.log("Hello, abbbbc world! abbc".replace(/ab+c/, "foobar")); // Hello, foobar world! abbc
-console.log("Hello, abbbbbc world! abbc".replace(/ab+c/g, "foobar")); // Hello, foobar world! foobar
+const str = "Hello, world";
+console.log(str.length); // 12
 
-const result = "Hello, abbbbc world! abc".match(/a(b+)c/);
-if (result !== null) {
-    console.log(result[0]); // abbbbc
-    console.log(result[1]); // bbbb
-}
+type HasLength = { length: number };
+const obj:HasLength = "foobar";
 
-const result1 = "Hello, abbbbbc world! abc".match(/a(?<worldName>b+)c/);
-if (result1 !== null) {
-    console.log(result1.groups); // [Object: null prototype] { worldName: 'bbbbb' }
-}
-
-const result2 = "Hello, abbbbbc world! abc".match(/a(b+)c/g);
-console.log(result2); // [ 'abbbbbc', 'abc' ]
+let val: {} = 123;
+val = "foobar";
+val = { num: 1234 };
+// val = null; // error
+// val = undefined; // error
