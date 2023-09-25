0c92f6611cfd6c419eb1a07f6f0d76c6fce4e4d4
tetsunari <mtst.tty556@gmail.com>
2023-09-25 23:56:09 +0900

    アロー関数

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,22 @@ const calcBMI2 = function({ height, weight }: Human): number {
     return weight / height ** 2;
 }
 console.log(calcBMI2(uhyo)); // 21.266540642722116
+
+const calcBMI3 = ({
+    height, weight
+}: Human): number => {
+    return weight / height ** 2;
+};
+
+const calcBMI4 = ({
+    height, weight
+}: Human): number => weight / height ** 2;
+
+type ReturnObj = {
+    bmi: number;
+};
+const calcBMIObject = ({
+    height, weight
+}: Human): ReturnObj => ({
+    bmi: weight / height ** 2
+});
