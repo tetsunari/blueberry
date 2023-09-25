6966a3ea893716b7dd6442d2ae1cad3247ce7031
tetsunari <mtst.tty556@gmail.com>
2023-09-25 23:29:52 +0900

    関数宣言

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,8 @@
-type User = {
-    name: string;
-    age: number;
-    premiumUser: boolean;
-}
-
-const data: string = `
-uhyo,26,1
-John Smith,17,0
-Mary SubmitEvent,14,1
-`;
-
-const users: User[] = [];
-const split = data.split("\n");
-for (const v of split) {
-    if (v === '') continue;
-
-    const [name, ageString, premiumUserString] = v.split(",");
-    const age = Number(ageString);
-    const premiumUser = premiumUserString === "1";
-
-    users.push({
-        name,
-        age,
-        premiumUser
-    })
-}
-
-for (const user of users) {
-    if (user.premiumUser) {
-        console.log(`${user.name} (${user.age}) is premium user.`);
-    } else {
-        console.log(`${user.name} (${user.age} is not premium user.)`)
+function range(min: number, max: number): number[] {
+    const result = [];
+    for (let i = min; i <= max; i++) {
+        result.push(i);
     }
+    return result;
 }
-
-/**
-uhyo (26) is premium user.
-John Smith (17 is not premium user.)
-Mary SubmitEvent (14) is premium user.
- */
+console.log(range(5, 10)); // [ 5, 6, 7, 8, 9, 10 ]
