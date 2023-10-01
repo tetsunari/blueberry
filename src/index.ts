a9c2759d181eef3b5d69eac375ba53c740c21a14
tetsunari <mtst.tty556@gmail.com>
2023-10-01 18:11:07 +0900

    コールバックのそれぞれのメソッド

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 type User = { name: string, age: number };
-const getName = (u: User): string => {
-    console.log("u is", u);
-    return u.name;
-};
 const users: User[] = [
-    { name: "uhyo", age: 26 },
-    { name: "John", age: 15,},
+    { name: "Uhyo", age: 26 },
+    { name: "john", age: 15 },
 ];
-const names = users.map(getName);
-console.log(names); 
-/**
- * u is { name: 'uhyo', age: 26 }
-u is { name: 'John', age: 15 }
-[ 'uhyo', 'John' ]
- */
+const names = users.map((u: User): string => u.name);
+console.log(names); // [ 'Uhyo', 'john' ]
+const adultUsers = users.filter((user: User) => user.age >= 20);
+console.log(adultUsers); // [ { name: 'Uhyo', age: 26 } ]
+const allAdult = users.every((user: User) => user.age >= 20);
+console.log(allAdult); // false
+const seniorExists = users.some((user: User) => user.age >= 60);
+console.log(seniorExists); // false
+const john = users.find((user: User) => user.name.startsWith("john"));
+console.log(john); // { name: 'john', age: 15 }
