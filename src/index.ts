57ed3885b185d0dced878c3803fc772cc5fcea7f
tetsunari <mtst.tty556@gmail.com>
2023-10-01 18:01:20 +0900

    コールバック関数

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-const toLowerOrUpper = (str: string, upper: boolean = false): string => {
-    if (upper) {
-        return str.toUpperCase();
-    } else {
-        return str.toLowerCase();
-    }
+type User = { name: string, age: number };
+const getName = (u: User): string => {
+    console.log("u is", u);
+    return u.name;
 };
-console.log(toLowerOrUpper("Hello")); // hello
-console.log(toLowerOrUpper("Hello", false)); // hello
-console.log(toLowerOrUpper("Hello", true)); // HELLO
+const users: User[] = [
+    { name: "uhyo", age: 26 },
+    { name: "John", age: 15,},
+];
+const names = users.map(getName);
+console.log(names); 
+/**
+ * u is { name: 'uhyo', age: 26 }
+u is { name: 'John', age: 15 }
+[ 'uhyo', 'John' ]
+ */
