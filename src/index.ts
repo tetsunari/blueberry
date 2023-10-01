1a1a89d05e97cb94c26ee07a4aef014af31ff3df
tetsunari <mtst.tty556@gmail.com>
2023-10-01 17:50:47 +0900

    オプショナル引数

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-const sum = (...args: number[]): number => {
-    let result = 0;
-    for (const num of args) {
-        result += num;
+const toLowerOrUpper = (str: string, upper?: boolean): string => {
+    if (upper) {
+        return str.toUpperCase();
+    } else {
+        return str.toLowerCase();
     }
-    return result;
 };
-const nums = [1,2,3,4,5];
-console.log(sum(...nums)); // 15
-
-const sum3 = (a: number, b: number, c: number) => a + b + c;
-const num_error = [1,2,3];
-// console.log(sum3(...num_error)); // error
-const num_good: [number, number, number] = [1,2,3];
-console.log(sum3(...num_good)); // 6
+console.log(toLowerOrUpper("Hello")); // hello
+console.log(toLowerOrUpper("Hello", false)); // hello
+console.log(toLowerOrUpper("Hello", true)); // HELLO
