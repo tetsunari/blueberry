c05a5bd2df47a96d6acf98d3973f18b425bca984
tetsunari <mtst.tty556@gmail.com>
2023-10-08 22:47:56 +0900

    ジェネリクス型

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-type UnaryFunc = (age: number) => number;
-type BinaryFunc = (left: number, right: number) => number;
-const double: UnaryFunc = age => age * 2;
-const add: BinaryFunc = (left, right) => left + right;
-const bin: BinaryFunc = double;
-console.log(bin(10, 100));
+type User<N> = {
+    name: N;
+}
