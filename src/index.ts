45738ab4837e0b03facf93cc1855f28d15e8a46e
tetsunari <mtst.tty556@gmail.com>
2023-10-24 23:45:45 +0900

    関数の型引数を宣言する方法

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-function repeat<T>(element: T, length: number): T[] {
+const repeat = function<T>(element: T, length: number): T[] {
     const result: T[] = [];
-    for(let i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         result.push(element);
     }
     return result;
 }
-console.log(repeat<string>("a", 5));
-console.log(repeat<number>(123, 3));
+
+const repeat2 = <T>(element: T, length: number): T[] => {
+    const result: T[] = [];
+    for (let i = 0; i < length; i++) {
+        result.push(element);
+    }
+    return result;
+}
+
+const utils = {
+    repeat3<T>(element: T, length: number): T[] {
+        const result: T[] = [];
+        for (let i = 0; i < length; i++) {
+            result.push(element);
+        }
+        return result;
+    }
+}
+console.log(repeat(1, 5));
+
+const pair = <Left, Right>(left: Left, right: Right): [Left, Right] => [left, right];
+const p = pair<string, number>("uhyo", 26);
