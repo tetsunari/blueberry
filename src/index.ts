e1aecdcc3051809a10436a82f89bfe7a627f5b9c
tetsunari <mtst.tty556@gmail.com>
2023-10-25 22:40:59 +0900

    関数の型引数省力

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-const repeat = <T extends {
-    name: string;
-}>(element: T, length: number): T[] => {
+function repeat<T>(element: T, length: number): T[] {
     const result: T[] = [];
-    for (let i = 0; i < length; i++) {
+    for(let i = 0; i < length; i++) {
         result.push(element);
     }
     return result;
 }
-
-type HasNameAndAge = {
-    name: string,
-    age: number,
-}
-console.log(repeat<HasNameAndAge>({
-    name: "uhyo",
-    age: 26,
-}, 3))
-
-// console.log(repeat<string>("a", 5)); // error
+const result = repeat("a", 5);
+console.log(result);
