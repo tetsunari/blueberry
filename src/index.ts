321f67825bd59a0be0e6a0fcb3dd07b3e4cd46d5
tetsunari <mtst.tty556@gmail.com>
2023-10-25 22:51:59 +0900

    型引数を持つ関数型

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-function repeat<T>(element: T, length: number): T[] {
+const repeat = function<T>(element: T, length: number): T[] {
     const result: T[] = [];
     for(let i = 0; i < length; i++) {
         result.push(element);
     }
     return result;
 }
-const result = repeat("a", 5);
-console.log(result);
+console.log(repeat(1, 5));
+
+type Func = <T>(arg: T, num: number) => T[];
+const repeat1: Func = (element, length) => {
+    const result = [];
+    for(let i = 0; i < length; i++) {
+        result.push(element);
+    }
+    return result;
+}
+console.log(repeat1("test", 5));
