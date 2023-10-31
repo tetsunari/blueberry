28efbd5d5f0765421017e7b1a00eafa94e56f78c
tetsunari <mtst.tty556@gmail.com>
2023-10-31 23:15:03 +0900

    ブロックスコープ

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const repeatLength = 5;
-const repeat = function<T>(element: T): T[] {
-    const repeatLength = 3;
-    const result: T[] = [];
-    for (let i = 0; i < repeatLength; i++) {
-        result.push(element);
+function sabayomi(age: number) {
+    if (age >= 30) {
+        const lie = age - 10;
+        return lie;
     }
-    return result;
+    if (age >= 20) {
+        const lie = age - 5;
+        return lie;
+    }
+    return age;
 }
-console.log(repeat("a")); // [ 'a', 'a', 'a' ]
-console.log(repeatLength); // 5
