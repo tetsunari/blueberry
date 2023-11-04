ff8361362eb4396b5f7c5c3af3fb99840cd815cd
tetsunari <mtst.tty556@gmail.com>
2023-11-04 21:01:08 +0900

    アクセスビリティ修飾子

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 class User {
-    static adminName: string = 'uhyo';
-    static getAdminUser() {
-        return new User(User.adminName, 24);
-    }
-
     name: string;
-    age: number;
+    private age: number;
 
     constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
     }
 
-    isAdult(): boolean {
+    public isAdult(): boolean {
         return this.age >= 20;
     }
 }
-console.log(User.adminName);
-const admin = User.getAdminUser();
-console.log(admin.name);
-console.log(admin.isAdult());
-const uhyo = new User("test", 26);
+const uhyo = new User("uhyo", 26);
 console.log(uhyo.name);
+console.log(uhyo.isAdult);
+console.log(uhyo.age); // error
