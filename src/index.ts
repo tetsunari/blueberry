597b6cee834910e6673910d8a917aa71ab8c3ab3
tetsunari <mtst.tty556@gmail.com>
2023-11-04 20:45:02 +0900

    コンストラクタ

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 class User {
-    name: string = '';
-    age: number = 0;
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
 
     isAdult(): boolean {
         return this.age >= 20;
     }
+}
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
+
+
+class readUser {
+    name: string;
+    readonly age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
 
     setAge(newAge: number) {
-        this.age = newAge;
+        this.age = newAge; // error
     }
 }
-const uhyo = new User();
-console.log(uhyo.isAdult()); // false
-uhyo.setAge(25);
-console.log(uhyo.isAdult()); // true
-const test = new User();
-test.age = 30;
-console.log(test.isAdult()); // true
+const readUhyo = new readUser("read", 26);
+readUhyo.age = 27; // error
