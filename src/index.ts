e8acb692d14ad3f80627a20819bb7a46b87d1689
tetsunari <mtst.tty556@gmail.com>
2023-11-05 18:36:51 +0900

    コンストラクタでプロパティを初期化

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 class User {
-    name: string;
-    private age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public isAdult(): boolean {
-        return this.age >= 20;
-    }
+    constructor(
+        public name: string,
+        private age: number
+    )
+    {}
 }
-const uhyo = new User("uhyo", 26);
-console.log(uhyo.name);
-console.log(uhyo.isAdult);
-console.log(uhyo.age); // error
