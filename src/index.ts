461397b3cb8ae0800bb8e594c2f828cd30dac30a
tetsunari <mtst.tty556@gmail.com>
2023-11-05 20:24:41 +0900

    instanceof

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-class User {
-    name: string = "";
-    age: number = 0;
+type HasName = {
+    age: number;
 }
-type MyYserConstructor = new () => User;
-const MyUser: MyYserConstructor = User;
-const u = new MyUser();
-console.log(u.name, u.age);
+class User {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+function getPrice(customer: HasName) {
+    if (customer instanceof User) {
+        if (customer.name === "uhyo") {
+            return 0;
+        }
+    }
+    return customer.age < 18 ? 1000 : 1800;
+}
+const customer1: HasName = { age: 15 };
+const customer2: HasName = { age: 40 };
+const uhyo = new User("uhyo", 26);
+console.log(getPrice(customer1)); // 1000
+console.log(getPrice(customer2)); // 1800
+console.log(getPrice(uhyo)); // 0
