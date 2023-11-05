f703d12b29ca3cc47838394973fdb49b5a9dff91
tetsunari <mtst.tty556@gmail.com>
2023-11-05 22:00:24 +0900

    privateとprotectedの使い所

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,51 @@
+// class User {
+//     name: string;
+//     private age: number;
+//     private _isAdult: boolean;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//         this._isAdult = this.age >= 20;
+//     }
+
+//     public isAdult(): boolean {
+//         return this._isAdult;
+//     }
+// }
+// class PremiumUser extends User {
+//     public isAdult(): boolean {
+//         return this.age >= 10; // error
+//     }
+// }
+// const miniUhyo = new PremiumUser("uhyo", 15);
+// const john = new User("john", 15);
+// console.log(miniUhyo.isAdult());
+// console.log(john.isAdult());
+
 class User {
     name: string;
-    #age: number;
+    protected age: number;
+    private _isAdult: boolean;
 
     constructor(name: string, age: number) {
         this.name = name;
-        this.#age = age;
+        this.age = age;
+        this._isAdult = age >= 20;
     }
 
     public isAdult(): boolean {
-        return this.#age >= 20;
+        return this._isAdult;
     }
 }
-class PremiumUser extends User {
-    rank: number = 1;
 
-    constructor(name: string, age: number, rank: number) {
-        // superはthis.の前に置く必要がある
-        super(name, age);
-        this.rank = rank;
+class PremiumUser extends User {
+    public setAge(newAge: number) {
+        this.age = newAge;
     }
 }
-const test = new PremiumUser("test", 15, 3);
-console.log(test.name);
-console.log(test.rank);
-console.log(test.isAdult());
+const uhyo = new PremiumUser("uhyo", 26);
+console.log(uhyo.isAdult()); // true
+uhyo.setAge(15);
+// _isAdultが更新されてないので正常に動作しない
+console.log(uhyo.isAdult()); // true
