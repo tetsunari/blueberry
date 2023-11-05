a1cb907d1924bcfcd879ce5a3686b46519b6d53a
tetsunari <mtst.tty556@gmail.com>
2023-11-05 18:50:45 +0900

    クラス式

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-class User {
-    constructor(
-        public name: string,
-        private age: number
-    )
-    {}
-}
+const User = class {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public isAdult(): boolean {
+        return this.age >= 20;
+    }
+};
+const uhyo = new User("uhyo", 25);
+console.log(uhyo.name);
+console.log(uhyo.isAdult());
