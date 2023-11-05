81e150ffd8ced4ea5559851ce6b613753bd3a233
tetsunari <mtst.tty556@gmail.com>
2023-11-05 19:27:22 +0900

    型引数を持つクラス

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,18 @@
-class User {
-    static adminUser: User;
-    static {
-        this.adminUser = new User;
-        this.adminUser.#age = 999;
-    }
+class User<T> {
+    name: string;
+    #age: number;
+    readonly data: T;
 
-    #age: number = 0;
-    getAge() {
-        return this.#age;
-    }
-
-    setAge(age: number) {
-        if (age < 0 || age > 150) {
-            return;
-        }
+    constructor(name: string, age: number, data: T) {
+        this.name = name;
         this.#age = age;
+        this.data = data;
     }
 }
-console.log(User.adminUser.getAge()); // 999
-const test = new User();
-console.log(test.getAge()); // 0
-test.setAge(100);
-console.log(test.getAge()); // 100
-test.setAge(1000);
-console.log(test.getAge()); // 100
+const uhyo = new User<string>("uhyo", 26, "追加データ");
+const data = uhyo.data;
+console.log(data); // 追加データ
+
+const john = new User("john", 10, { num: 123 });
+const data1 = john.data;
+console.log(data1); // { num: 123 }
