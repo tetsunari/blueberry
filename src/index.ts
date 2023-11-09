fbd81e034410c633c518257e6bce5ca4d17abafb
tetsunari <mtst.tty556@gmail.com>
2023-11-09 23:31:00 +0900

    thisを操作するメソッド

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,15 @@ class User {
         this.#age = age;
     }
 
-    public isAdult(): boolean {
+    public isAdult() {
         return this.#age >= 20;
     }
-
-    public filterOlder(users: readonly User[]): User[] {
-        return users.filter(u => u.#age > this.#age);
-    }
-
-    public filterOlder2(users: readonly User[]): User[] {
-        // error: this implicitly has type any because it does not have a type annotation.ts(2683)
-        return users.filter(function(u) {return u.#age > this.#age;});
-    }
-
-    public filterOlder3(users: readonly User[]): User[] {
-        return users.filter(function(this: User, u) {return u.#age > this.#age;});
-    }
-
-    public filterOlder4(users: readonly User[]): User[] {
-        const _this = this;
-        return users.filter(function(u) {return u.#age > _this.#age;});
-    }
 }
-const uhyo = new User("uhyo", 25);
-const john = new User('john', 15);
-const bob = new User('bob', 30);
-const older = uhyo.filterOlder([john, bob]);
-console.log(older);  // [ User { name: 'bob' } ]
+const uhyo = new User('uhyo', 25);
+const john = new User('John', 17);
+console.log(uhyo.isAdult()); // true
+console.log(uhyo.isAdult.apply(john, [])); // false
+
+const boundIsAdult = uhyo.isAdult.bind(uhyo);
+console.log(boundIsAdult()); // true
+console.log(boundIsAdult.call(john)); // true
