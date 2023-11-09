cc17f2be571fe604452416ae900e76b52ac94a12
tetsunari <mtst.tty556@gmail.com>
2023-11-09 22:26:57 +0900

    implements

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,35 @@
-// class User {
-//     name: string;
-//     private age: number;
-//     private _isAdult: boolean;
-
-//     constructor(name: string, age: number) {
-//         this.name = name;
-//         this.age = age;
-//         this._isAdult = this.age >= 20;
-//     }
-
-//     public isAdult(): boolean {
-//         return this._isAdult;
-//     }
-// }
-// class PremiumUser extends User {
-//     public isAdult(): boolean {
-//         return this.age >= 10; // error
-//     }
-// }
-// const miniUhyo = new PremiumUser("uhyo", 15);
-// const john = new User("john", 15);
-// console.log(miniUhyo.isAdult());
-// console.log(john.isAdult());
-
-class User {
+type HasName = {
     name: string;
-    protected age: number;
-    private _isAdult: boolean;
+}
+
+class User implements HasName {
+    name: string;
+    #age: number;
 
     constructor(name: string, age: number) {
         this.name = name;
-        this.age = age;
-        this._isAdult = age >= 20;
+        this.#age = age;
     }
 
     public isAdult(): boolean {
-        return this._isAdult;
+        return this.#age >= 20;
     }
 }
 
-class PremiumUser extends User {
-    public setAge(newAge: number) {
-        this.age = newAge;
+
+type HasNameError = {
+    name: string;
+}
+
+// Error: Property '#age' is missing in type 'UserError' but required in type 'User'.
+class UserError implements HasNameError {
+    #age: number;
+
+    constructor(age: number) {
+        this.#age = age;
+    }
+
+    public isAdult(): boolean {
+        return this.#age >= 20;
     }
 }
-const uhyo = new PremiumUser("uhyo", 26);
-console.log(uhyo.isAdult()); // true
-uhyo.setAge(15);
-// _isAdultが更新されてないので正常に動作しない
-console.log(uhyo.isAdult()); // true
