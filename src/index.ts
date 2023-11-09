e635cee8a79bfa41def4ec70a092cb3d0607eafa
tetsunari <mtst.tty556@gmail.com>
2023-11-09 22:39:26 +0900

    this

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-type HasName = {
-    name: string;
-}
-
-class User implements HasName {
-    name: string;
-    #age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.#age = age;
+const User = {
+    name: "uhyo",
+    age: 26,
+    isAdult() {
+        return this.age >= 20;
     }
-
-    public isAdult(): boolean {
-        return this.#age >= 20;
-    }
-}
-
-
-type HasNameError = {
-    name: string;
-}
-
-// Error: Property '#age' is missing in type 'UserError' but required in type 'User'.
-class UserError implements HasNameError {
-    #age: number;
-
-    constructor(age: number) {
-        this.#age = age;
-    }
-
-    public isAdult(): boolean {
-        return this.#age >= 20;
-    }
-}
+};
+console.log(User.isAdult()); // true
+User.age = 19;
+console.log(User.isAdult()); // false
