36e2b1eb0760d5aa82dee7a4115a621eb2ddbe89
tetsunari <mtst.tty556@gmail.com>
2023-11-09 23:05:51 +0900

    アロー関数

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-const User = {
-    name: "uhyo",
-    age: 26,
-    isAdult() {
-        return this.age >= 20;
+class User {
+    name: string;
+    #age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.#age = age;
     }
-};
-console.log(User.isAdult()); // true
-User.age = 19;
-console.log(User.isAdult()); // false
+
+    public isAdult(): boolean {
+        return this.#age >= 20;
+    }
+
+    public filterOlder(users: readonly User[]): User[] {
+        return users.filter(u => u.#age > this.#age);
+    }
+
+    public filterOlder2(users: readonly User[]): User[] {
+        // error: this implicitly has type any because it does not have a type annotation.ts(2683)
+        return users.filter(function(u) {return u.#age > this.#age;});
+    }
+
+    public filterOlder3(users: readonly User[]): User[] {
+        return users.filter(function(this: User, u) {return u.#age > this.#age;});
+    }
+
+    public filterOlder4(users: readonly User[]): User[] {
+        const _this = this;
+        return users.filter(function(u) {return u.#age > _this.#age;});
+    }
+}
+const uhyo = new User("uhyo", 25);
+const john = new User('john', 15);
+const bob = new User('bob', 30);
+const older = uhyo.filterOlder([john, bob]);
+console.log(older);  // [ User { name: 'bob' } ]
