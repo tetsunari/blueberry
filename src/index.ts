0d60b3ead5acf7f44442c8b762b59f99e0a3df35
tetsunari <mtst.tty556@gmail.com>
2023-11-18 21:36:18 +0900

    ユニオン型

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
-class User {
-    readonly name: string
-    readonly age: number
-
-    constructor(name: string, age: number) {
-        if (name === '') {
-            throw new Error('名前を空にはできません！')
-        }
-        this.name = name
-        this.age = age
-    }
-
-    getMessage(message: string): string {
-        return `${this.name} (${this.age}) 「${message}」`
-    }
+type Animal = {
+    species: string;
 }
-
-const uhyo = new User('uhyo', 30);
-console.log(uhyo.getMessage('こんにちは！'));
-
-function createUser(name: string, age: number) {
-    return (message: string) => {
-        return `${name} (${age}) 「${message}」`
-    }
+type Human = {
+    name: string;
 }
-const getMessage = createUser('uhyo', 30);
-console.log(getMessage('こんにちは！'));
+type User = Animal | Human;
+const tama: User = {
+    species: "Felis silvestris catus"
+}
+const uhyo: User = {
+    name: "uhyo"
+}
