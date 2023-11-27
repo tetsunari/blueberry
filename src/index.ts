474972e36d2e774d61c9b607be1a668f545caa1f
tetsunari <mtst.tty556@gmail.com>
2023-11-28 00:00:39 +0900

    テンプレートリテラル型

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-type FooString = "foo";
-const foo: FooString = "foo";
-const bar: FooString = "bar"; // Error: Type '"bar"' is not assignable to type '"foo"'.
+function getHelloStr(): `Hello, ${string}!` {
+    const rand = Math.random();
+    if (rand < 0.3) {
+        return "Hello, World!";
+    } else if (rand < 0.6) {
+        return "Hello, my World!";
+    } else if (rand < 0.9) {
+        // Hello, world!でないのでエラー
+        return "Hello, world.";
+    } else {
+        // Hello, World!でないのでエラー
+        return "Hell, World!";
+    }
+}
 
-const foo2: "foo2" = "foo2";
-const one: 1 = 1;
-const t: true = true;
-const three: 3n = 3n;
+function  makeKey<T extends string>(UserName: T) {
+    return `user:${UserName}` as const;
+}
+const uyoKey: "user:uhyo" = makeKey("uhyo");
 
-const uhyoName = "uhyo";
-// uhyoNameは"uhyo"という文字列リテラル型になる
-const age = 30;
-// ageは30という数値リテラル型になる
+function fromKey<T extends string>(key: `user:${T}`): T {
+    return key.slice(5) as T;
+}
+const user = fromKey("user:uhyo");
