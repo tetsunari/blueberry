2d5477214b49a7b60e051a1f724cd03630aca27d
tetsunari <mtst.tty556@gmail.com>
2023-11-27 23:06:10 +0900

    オプショナルチェイン

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 type Human = {
     name: string;
-    age?: number;
+    age: number;
 };
-const uhyo: Human = {
-    name: "uhyo",
-    age: 25,
-};
-const john: Human = {
-    name: "john",
-};
-const unde: Human = {
-    name: "unde",
-    age: undefined,
-};
+function useMaybeHuman(human: Human | null) {
+    const age = human?.age;
+    console.log(age);
+    // age is number | undefined
+}
+
+type GetTimeFunc = () => Date;
+function useTime(getTimeFunc: GetTimeFunc | undefined) {
+    const timeOrUndefined = getTimeFunc?.();
+    // timeOrUndefined is Date | undefined
+}
+
+type User = {
+    isAdult(): boolean;
+}
+function checkForAdultUser(user: User | null) {
+    if (user?.isAdult()) {
+        showSpecilContents(user);
+    }
+}
+
+type GetTimeFunc2 = () => Date;
+function useTime2(getTimeFunc: GetTimeFunc2 | undefined) {
+    const time = getTimeFunc?.().toString();
+}
