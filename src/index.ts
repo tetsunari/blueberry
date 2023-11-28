a991e9654bdfe3970779ef8bb0f4f8f48734ddc8
tetsunari <mtst.tty556@gmail.com>
2023-11-28 23:56:27 +0900

    ユニオン型とリテラル型の組み合わせ

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-function getHelloStr(): `Hello, ${string}!` {
-    const rand = Math.random();
-    if (rand < 0.3) {
-        return "Hello, World!";
-    } else if (rand < 0.6) {
-        return "Hello, my World!";
-    } else if (rand < 0.9) {
-        // Hello, world!でないのでエラー
-        return "Hello, world.";
-    } else {
-        // Hello, World!でないのでエラー
-        return "Hell, World!";
-    }
+function signNumber(type: "plus" | "minus") {
+    return type === "plus" ? 1 : -1;
 }
 
-function  makeKey<T extends string>(UserName: T) {
-    return `user:${UserName}` as const;
-}
-const uyoKey: "user:uhyo" = makeKey("uhyo");
-
-function fromKey<T extends string>(key: `user:${T}`): T {
-    return key.slice(5) as T;
-}
-const user = fromKey("user:uhyo");
+console.log(signNumber("plus")); // 1
+console.log(signNumber("minus")); // -1
+console.log(signNumber("uhyo")); // error
