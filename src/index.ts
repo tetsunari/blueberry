d875f13c53cb628fedf5ffa965d79a716ab3fb04
tetsunari <mtst.tty556@gmail.com>
2023-12-09 16:41:25 +0900

    wideningされるリテラル型・されないリテラル型

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,4 @@
-const uhyo1 = "uhyo";
-// uhyo1は"uhyo"型
-let uhyo2 = "uhyo";
-// uhyo2はstring型
-
-let uhyo: string|number = "uhyo";
-uhyo = "john";
-uhyo = 3.14;
-
-let uhyo3: "uhyo"|"john" = "uhyo";
-
-const uhyo4 = {
-    name: "uhyo",
-    age: 26
-};
-uhyo4.name = "john";
-
-type Human = {
-    readonly name: string;
-    readonly age: number;
-}
-const uhyo5: Human = {
-    name: "uhyo",
-    age: 26
-};
-
-type Uhyo = {
-    name: "uhyo",
-    age: number
-};
-const uhyo6: Uhyo = {
-    name: "uhyo",
-    age: 26
-};
-
-function signNumber(type: "plus" | "minus") {
-    return type === "plus" ? 1 : -1;
-}
-function useNumber(num: number) {
-    return num > 0 ? "plus" : num < 0 ? "minus" : "zero";
-}
-signNumber("uhyo"); // Argument of type '"uhyo"' is not assignable to parameter of type '"plus" | "minus"'.
-useNumber("uhyo"); // Argument of type '"uhyo"' is not assignable to parameter of type 'number'.
+const uhyo1 = "uhyo1"; // wideningされるuhyo型
+const uhyo2: "uhyo" = "uhyo"; // wideningされないuhyo型
+let uhyo3 = "uhyo1"; // string型
+let uhyo4 = uhyo2; // "uhyo"型
