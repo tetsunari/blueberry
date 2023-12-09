357ee1adbe120076cc7a2aafdcf84be30d2a6a04
tetsunari <mtst.tty556@gmail.com>
2023-12-09 18:08:54 +0900

    lookup型

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,37 @@
-type Animal = {
-    tag: "animal";
-    species: string;
-}
 type Human = {
-    tag: "human";
+    type: "human";
     name: string;
+    age: number;
+};
+function setAge(human: Human, age: Human["age"]) {
+    return {
+        ...human,
+        age
+    };
 }
-type User = Animal|Human;
+const uhyo: Human = {
+    type: "human",
+    name: "uhyo",
+    age: 26
+};
+const uhyo2 = setAge(uhyo, 27);
+console.log(uhyo2); // { type: 'human', name: 'uhyo', age: 27 }
 
-function getUserName(user: User): string {
-    switch (user.tag) {
-        case "human":
-            return user.name;
-        case "animal":
-            return "名無し";
-    }
-}
-
-type Robot = {
-    tag: "robot",
+type Human2 = {
+    type: "human";
     name: string;
+    age: bigint;
+};
+function setAge2(human: Human2, age: Human2["age"]) {
+    return {
+        ...human,
+        age
+    };
 }
-type User2 = Animal|Human|Robot;
-
-function getUserName2(user: User2): string {
-    switch (user.tag) {
-        case "human":
-            return user.name;
-        case "animal":
-            return "名無し";
-        case "robot":
-            return user.name;
-    }
-}
+const uhyo3: Human2 = {
+    type: "human",
+    name: "uhyo",
+    age: 26n,
+};
+const uhyo4 = setAge2(uhyo3, 27n);
+console.log(uhyo4); // { type: 'human', name: 'uhyo', age: 27n }
