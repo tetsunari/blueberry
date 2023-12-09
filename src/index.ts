4307630ede89a22a5723cc7f2b8b46ccae9d5183
tetsunari <mtst.tty556@gmail.com>
2023-12-09 17:03:52 +0900

    等価演算子

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-const uhyo1 = "uhyo1"; // wideningされるuhyo型
-const uhyo2: "uhyo" = "uhyo"; // wideningされないuhyo型
-let uhyo3 = "uhyo1"; // string型
-let uhyo4 = uhyo2; // "uhyo"型
+type SignType = "plus" | "minus";
+function signNumber(type: SignType) {
+    return type === "plus" ? 1 : -1;
+}
+function numberWithSign(number: number, type: SignType|"none") {
+    if (type === "none") {
+        return 0;
+    } else {
+        return number * signNumber(type);
+    }
+}
+console.log(numberWithSign(5, "plus")); // 5
+console.log(numberWithSign(5, "minus")); // -5
+console.log(numberWithSign(5, "none")); // 0
