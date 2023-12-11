24321acacd6ef65c1cf7475a60fe6e221af2f2bf
tetsunari <mtst.tty556@gmail.com>
2023-12-12 00:20:00 +0900

    型アサーション

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-type Obj = {
-    0: string;
-    1: number;
+function getFirstFiveLetters(strOrNum: string|number) {
+    const str = strOrNum as string;
+    return str.slice(0, 5);
 }
-const obj: Obj = {
-    0: "uhyo",
-    "1": 26
-};
-obj["0"] = "john";
-obj[1] = 15;
-type ObjKeys = keyof Obj;
+console.log(getFirstFiveLetters("uhyouhyouhyo"));
+console.log(getFirstFiveLetters(123));
 
-function get<T, K extends keyof T & string>(obj: T, key: K): T[K] {
-    const keyName: string = key;
-    return obj[key];
+type Animal = {
+    tag: "animal";
+    species: string;
+}
+type Human = {
+    tag: "human";
+    name: string;
+}
+type User = Animal|Human;
+function getNamesIfAllHuman(users: readonly User[]): string[]|undefined {
+    if (users.every(user => user.tag === "human")) {
+        return (users as Human[]).map(user => user.name);
+    }
+    return undefined;
 }
