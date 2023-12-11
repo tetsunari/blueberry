52445fc6be94735563f5fdee7ec8652261898b8a
tetsunari <mtst.tty556@gmail.com>
2023-12-11 23:36:01 +0900

    number型もキーになれる

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-function get<T, K extends keyof T>(obj: T, key: K): T[K] {
+type Obj = {
+    0: string;
+    1: number;
+}
+const obj: Obj = {
+    0: "uhyo",
+    "1": 26
+};
+obj["0"] = "john";
+obj[1] = 15;
+type ObjKeys = keyof Obj;
+
+function get<T, K extends keyof T & string>(obj: T, key: K): T[K] {
+    const keyName: string = key;
     return obj[key];
 }
-type Human = {
-    name: string;
-    age: number;
-}
-const uhyo: Human = {
-    name: "uhyo",
-    age: 26,
-};
-const uhyoName = get(uhyo, "name");
-const uhyoAge = get(uhyo, "age");
-console.log(uhyoName); // "uhyo"
-console.log(uhyoAge); // 26
