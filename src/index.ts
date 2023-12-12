071398f074e722e161b5100fbad5396094dd8e44
tetsunari <mtst.tty556@gmail.com>
2023-12-13 00:22:50 +0900

    nullアサーション

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-function getFirstFiveLetters(strOrNum: string|number) {
-    const str = strOrNum as string;
-    return str.slice(0, 5);
-}
-console.log(getFirstFiveLetters("uhyouhyouhyo"));
-console.log(getFirstFiveLetters(123));
-
-type Animal = {
-    tag: "animal";
-    species: string;
-}
 type Human = {
-    tag: "human";
     name: string;
+    age: number;
 }
-type User = Animal|Human;
-function getNamesIfAllHuman(users: readonly User[]): string[]|undefined {
-    if (users.every(user => user.tag === "human")) {
-        return (users as Human[]).map(user => user.name);
+function getOneUserName(user1?: Human, user2?: Human): string|undefined {
+    if (user1 === undefined && user2 === undefined) {
+        return undefined;
     }
-    return undefined;
+    if (user1 !== undefined) {
+        return user1.name;
+    }
+    return user2!.name;
+}
+
+function showOneUserName(user1?: Human, user2?: Human): string|undefined {
+    if (user1 === undefined && user2 === undefined) {
+        return undefined;
+    }
+    if (user1 !== undefined) {
+        return user1.name;
+    }
+    return (user2 as Human).name;
+}
+
+function showOneUserName1(user1?: Human, user2?: Human): string|undefined {
+    return user1?.name ?? user2?.name;
 }
