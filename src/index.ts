ef52e222887625f2674600f97564e2d952835ef6
tetsunari <mtst.tty556@gmail.com>
2023-12-21 23:53:48 +0900

    as const

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
-type Human = {
-    name: string;
-    age: number;
-}
-function getOneUserName(user1?: Human, user2?: Human): string|undefined {
-    if (user1 === undefined && user2 === undefined) {
-        return undefined;
-    }
-    if (user1 !== undefined) {
-        return user1.name;
-    }
-    return user2!.name;
-}
-
-function showOneUserName(user1?: Human, user2?: Human): string|undefined {
-    if (user1 === undefined && user2 === undefined) {
-        return undefined;
-    }
-    if (user1 !== undefined) {
-        return user1.name;
-    }
-    return (user2 as Human).name;
-}
-
-function showOneUserName1(user1?: Human, user2?: Human): string|undefined {
-    return user1?.name ?? user2?.name;
-}
+const names1 = ["uhyo", "john", "Taro"];
+// string[]型
+const names2 = ["uhyo", "john", "Taro"] as const;
+// readonly ["uhyo", "john", "Taro"]型
+const names = ["uhyo", "john", "Taro"] as const;
+type Name = (typeof names)[number];
+// 上と同じ
+// type Name = "uhyo" | "john" | "Taro";
+// const names: Name[] = ["uhyo", "john", "Taro"];
