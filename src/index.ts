63b81a203f06af89e2757983dc4b1980522c907f
tetsunari <mtst.tty556@gmail.com>
2023-12-25 22:50:47 +0900

    unknown型

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-function doWhatever(obj: any) {
-    // 好きなプロパティにアクセスできる
-    console.log(obj.user.name);
-    // 関数も呼び出せる
-    obj();
-    // 計算もできる
-    const result = obj * 10;
-    return result;
+function doNothing(val: unknown) {
+    console.log(val);
 }
-
-// 全部コンパイルエラーが発生しないがランタイムエラーが発生する
-doWhatever(3);
-doWhatever({
+doNothing(3);
+doNothing({
     user: {
-        name: 'John'
+        name: "uhyo"
     }
 });
-doWhatever(() => {
-    console.log('hello');
-});
+doNothing(() => {
+    console.log("hi");
+})
 
-function useNumber(num: number) {
-    console.log(num);
+function doNoting2(val: unknown) {
+    // Object is of type 'unknown'.
+    // const name = val.name;
+    // console.log(name);
 }
-function doWhatever2(obj: any) {
-    // string型の変数に代入できる
-    const str: string = obj;
-    // number型の変数に代入できる
-    useNumber(obj);
+
+function useUnknown(val: unknown) {
+    if (typeof val === "string") {
+        console.log("valは文字列です。");
+        console.log(val.slice(0, 5));
+    } else {
+        console.log("valはstring型ではありません。");
+        console.log(val);
+    }
 }
+useUnknown("foobar");
+useUnknown(null);
