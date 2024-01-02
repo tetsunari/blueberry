e8a66cdd317818239c26c319382f7b34476fdeaf
tetsunari <mtst.tty556@gmail.com>
2024-01-02 18:07:05 +0900

    組み込み型

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-type RestArgs<M> = M extends "string" ? [string, string] : [number, number, number];
-function func<M extends "string" | "number">(mode: M , ...args: RestArgs<M>) {
-    console.log(mode, ...args);
-}
-
-func("string", "uhyo", "hyo");
-func("number", 1, 2, 3);
-
-// func("string", 1, 2);
-// error: Argument of type 'number' is not assignable to parameter of type 'string'.
-// func("number", "uhyo", "hyo");
-// error: Argument of type 'string' is not assignable to parameter of type 'number'.
+type T = Readonly<{
+    name: string;
+    age: number;
+}>;
+type T1 = Partial<{
+    name: string;
+    age: number;
+}>;
+type T2 = Pick<{
+    name: string;
+    age: Number;
+}, "age">;
+type Union = "uhyo" | "hyo" | 1 | 2 | 3;
+type T3 = Extract<Union, string>;
+type T4 = Exclude<Union, string>;
