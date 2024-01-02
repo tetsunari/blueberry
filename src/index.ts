06853ba3b083e7ce578f4111f0353445aad5c23a
tetsunari <mtst.tty556@gmail.com>
2024-01-02 18:54:42 +0900

    タグ付きユニオン

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,28 @@ type Option <T> = {
 };
 
 function showNumberIfExists(obj: Option<number>): void {
-    if (obj.tag === "some") {
+    if (isSome(obj)) {
         console.log(obj.value);
     }
 }
-const four: Option<number> = {
-    tag: "some",
-    value: 4
-};
-const nothing: Option<number> = {
-    tag: "none"
-};
-showNumberIfExists(four); // 4
-showNumberIfExists(nothing); // nothing
+
+function isSome<T>(obj: Option<T>): obj is { tag: "some", value: T } {
+    return obj.tag === "some";
+}
+// other way
+// function isSome<T>(obj: Option<T>): obj is Extract<Option<T>, { tag: "some" }> {
+//     return obj.tag === "some";
+// }
+
+// other way
+// type Some<T> = {
+//     tag: "some";
+//     value: T;
+// }
+// type None = {
+//     tag: "none";
+// }
+// type Option<T> = Some<T> | None;
+// function isSome<T>(obj: Option<T>): obj is Some<T> {
+//     return obj.tag === "some";
+// }
