263d15706b04bb3d27a8f22d3e248ec8cf9cc322
tetsunari <mtst.tty556@gmail.com>
2024-01-02 18:13:22 +0900

    anyをisに置き換える

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-type T = Readonly<{
-    name: string;
-    age: number;
-}>;
-type T1 = Partial<{
-    name: string;
-    age: number;
-}>;
-type T2 = Pick<{
-    name: string;
-    age: Number;
-}, "age">;
-type Union = "uhyo" | "hyo" | 1 | 2 | 3;
-type T3 = Extract<Union, string>;
-type T4 = Exclude<Union, string>;
+type Human = {
+    type: "Human",
+    name: string,
+    age: number
+};
+function isHuman(value: any): value is Human {
+    if (value === null) return false;
+    return (
+        value.type === "Human" &&
+        typeof value.name === "string" &&
+        typeof value.age === "number"
+    )
+}
+
+function isPropertyAccess(value: unknown): value is { [key: string]: unknown } {
+    return value != null;
+}
+function isHuman1(value: unknown): value is Human {
+    if (!isPropertyAccess(value)) return false;
+    return (
+        value.type === "Human" &&
+        typeof value.name === "string" &&
+        typeof value.age === "number"
+    )
+};
