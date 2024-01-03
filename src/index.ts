5f26021fbfd3f86d30ac0f0caca256663315aebf
tetsunari <mtst.tty556@gmail.com>
2024-01-03 23:46:54 +0900

    export, import

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-type Option<T> = { tag: "Some"; value: T } | { tag: "None" };
-function doubleOption(obj: Option<number>) {
-    return mapOption(obj, x => x * 2);
-}
-const four: Option<number> = { tag: "Some", value: 4 };
-const none: Option<number> = { tag: "None" };
-console.log(doubleOption(four)); // { tag: "Some", value: 8 }
-console.log(doubleOption(none)); // { tag: "None" }
+import { name, age } from "./uhyo.js";
 
-function mapOption<T, U>(obj: Option<T>, callback: (value: T) => U): Option<U> {
-    switch (obj.tag) {
-        case "Some":
-            return { tag: "Some", value: callback(obj.value) };
-        case "None":
-            return { tag: "None" };
-    }
-}
+console.log(age); // 26
+console.log(`uhyoの名前は${name}で, 年齢は${age}です`); // uhyoの名前はuhyoで, 年齢は26です
