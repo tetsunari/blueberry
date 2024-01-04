650771c02e530391aaaacc02290bd863747a4cd9
tetsunari <mtst.tty556@gmail.com>
2024-01-04 23:26:51 +0900

    関数のexport

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import { uhyoName, age as uhyoAge } from "./uhyo.js";
-
-console.log(`uhyoの名前は${uhyoName}です。年齢は${uhyoAge}です。`); // uhyoの名前はuhyoです。年齢は26です。
+import { getUhyoName } from "./uhyo.js";
+console.log(`uhyoの名前は${getUhyoName()}です`);
