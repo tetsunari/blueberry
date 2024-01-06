3b6727325c9218c98ab826a5ea5d161b8a401859
tetsunari <mtst.tty556@gmail.com>
2024-01-06 23:24:41 +0900

    node.jsの組み込みモジュール

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import type { Human } from './human.js';
-export const uhyo: Human = {
-    name: 'uhyo',
-    age: 26,
-};
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('文字列を入力してください：', (line) => {
+    console.log(`${line}が入力されました。`);
+    rl.close();
+})
