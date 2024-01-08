4a4bb087f81b3448812e621e4e245f6780308b04
tetsunari <mtst.tty556@gmail.com>
2024-01-08 20:55:42 +0900

    非同期のファイル操作

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-setTimeout(() => {
-    console.log("タイマーが呼び出されました");
-}, 3000);
-console.log(" タイマーを設定しました");
+import { readFile } from "fs";
+import { performance } from "perf_hooks";
+
+const startTime = performance.now();
+readFile("foo.txt", "utf8", (err, result) => {
+    const endTime = performance.now();
+    console.log(result);
+    console.log(`処理時間: ${endTime - startTime}ミリ秒`);
+});
+console.log("読み込み開始");
