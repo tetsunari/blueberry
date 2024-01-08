0ae4a472a6549a77d7d0fb25ce18dc50ca687de3
tetsunari <mtst.tty556@gmail.com>
2024-01-08 21:02:20 +0900

    同期処理と非同期処理の順序

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import { readFile } from "fs";
 import { performance } from "perf_hooks";
 
-const startTime = performance.now();
-readFile("foo.txt", "utf8", (err, result) => {
-    const endTime = performance.now();
-    console.log(result);
-    console.log(`処理時間: ${endTime - startTime}ミリ秒`);
-});
-console.log("読み込み開始");
+setTimeout(() => {
+    console.log("タイマーが呼び出されました");
+}, 1000);
+
+const start = performance.now();
+let count = 0;
+while (performance.now() - start < 1000) {
+    count++;
+}
+console.log(`ループを${count}回実行しました`);
+// ループをn回実行しました
+// タイマーが呼び出されました
+// 同期処理が先に実行される
