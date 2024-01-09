c4d78c9c0e94460122afc3128fdb9a34d0545d09
tetsunari <mtst.tty556@gmail.com>
2024-01-10 00:20:38 +0900

    コールバック関数の登録とエラー処理2

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,11 @@ import { readFile } from "fs/promises";
 
 const p = readFile("foo.txt", "utf8");
 p.then((result) => {
-    console.log(1);
-});
-p.then((result) => {
-    console.log(2);
-});
-p.then((result) => {
-    console.log(3);
-});
-// 1 2 3
-
-p.then((result) => {
     console.log("成功", result);
 });
-p.catch((error) => {
-    console.log("失败", error);
-});
-
-// 成功時と失敗時の両方を処理する
-p.then((result) => {
-    console.log("成功", result);
-}, (error) => {
-    console.log("失败", error);
-});
-
-// errorの型がわからないので、anyではなくunknownを使う
-p.then((result) => {
-    console.log("成功", result);
-}, (error: unknown) => {
-    console.log("失败", error);
-});
+p.catch((error: unknown) => {
+    console.log("失敗", error);
+})
+p.finally(() => {
+    console.log("完了");
+})
