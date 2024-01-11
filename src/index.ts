7923c32b88aef2339fa5f39c424a57859e0aecee
tetsunari <mtst.tty556@gmail.com>
2024-01-12 00:06:29 +0900

    Promise all

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
-const p = Promise.resolve(100);
-p.then((result) => {
-    console.log(`result is ${result}`);
+import { readFile } from "fs/promises";
+
+const pFoo = readFile("foo.txt", "utf8");
+const pBar = readFile("bar.txt", "utf8");
+const pBaz = readFile("baz.txt", "utf8");
+const p = Promise.all([pFoo, pBar, pBaz]);
+p.then((results) => {
+    console.log("foo.txt:", results[0]);
+    console.log("bar.txt:", results[1]);
+    console.log("bax.txt:", results[2]);
 });
-// Promise.resolve(100) は、以下と同じ意味
-new Promise((resolve) => { resolve(100); })
 
-// Promise.reject("foo") は、以下と同じ意味
-new Promise((resolve, reject) => { reject("foo"); })
-
-const p1 = Promise.resolve(100);
-p.then(() => {
-    console.log("2");
-})
-console.log("1");
-// 1 2
+// 上と同じ
+const p2 = Promise.all([
+    readFile("foo.txt", "utf8"),
+    readFile("bar.txt", "utf8"),
+    readFile("baz.txt", "utf8"),
+]);
+p2.then((results) => {
+    const [foo, bar, baz] = results;
+    console.log("foo.txt:", foo);
+    console.log("bar.txt:", bar);
+    console.log("baz.txt:", baz);
+});
+// 上と同じ
+p2.then(([foo, bar, baz]) => {
+    console.log("foo.txt:", foo);
+    console.log("bar.txt:", bar);
+    console.log("baz.txt:", baz);
+});
+/**
+ * foo.txt: aiueo
+ * bar.txt: bar
+ * bax.txt: baz
+ */
