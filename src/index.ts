c424d7e5150ec27f25e0dbf8eddd79b81813a708
tetsunari <mtst.tty556@gmail.com>
2024-01-12 23:40:50 +0900

    promise allSettled

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 import { readFile } from "fs/promises";
 
-const p = Promise.race([
-    readFile("foo.txt", "utf8"),
-    readFile("bar.txt", "utf8"),
-    readFile("baz.txt", "utf8"),
-]);
-p.then((result) => {
-    console.log(result);
-});
-
 const sleepReject = (duration: number) => {
     return new Promise<never>((resolve, reject) => {
         setTimeout(reject, duration);
     })
 };
-const p2 = Promise.race([
+const p = Promise.allSettled([
     readFile("foo.txt", "utf8"),
-    sleepReject(1000),
+    sleepReject(5000),
 ]);
-p2.then((result) => {
-    console.log("success", result);
-}, (error: unknown) => {
-    console.log("error", error);
+p.then((result) => {
+    console.log(result);
+    // [
+    //   { status: 'fulfilled', value: 'aiueo' },
+    //   { status: 'rejected', reason: undefined }
+    // ]
 })
