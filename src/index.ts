f5fbb2a05b5bd1181d6c34e87ffd20dbd8c07070
tetsunari <mtst.tty556@gmail.com>
2024-01-17 23:10:37 +0900

    awaitの実行順序

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,23 @@ const sleep = (duration: number) => {
     });
 };
 async function get3() {
+    console.log("get3() called");
     await sleep(1000);
+    console.log("get3() finished");
     return 3;
 }
+console.log("before get3()");
 const p = get3();
 p.then(num => {
     console.log(`num is ${num}`);
 });
+console.log("after get3()");
+
+/**
+ * before get3()
+ * get3() called
+ * after get3()
+ * 1秒後に
+ * get3() finished
+ * num is 3
+ */
