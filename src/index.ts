2bbe8af5c020a09b5aedcbb8c9798cf26fe2fa73
tetsunari <mtst.tty556@gmail.com>
2024-01-17 23:36:54 +0900

    awaitの返り値

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,26 @@ const sleep = (duration: number) => {
     });
 };
 async function get3() {
-    console.log("get3() called");
     await sleep(1000);
-    console.log("get3() finished");
     return 3;
 }
-console.log("before get3()");
-const p = get3();
-p.then(num => {
-    console.log(`num is ${num}`);
-});
-console.log("after get3()");
+async function main() {
+    const num1 = await get3();
+    const num2 = await get3();
+    const num3 = await get3();
+    return num1 + num2 + num3;
+}
+main().then((result => {
+    console.log(`result is ${result}`);
+}))
 
-/**
- * before get3()
- * get3() called
- * after get3()
- * 1秒後に
- * get3() finished
- * num is 3
- */
+async function main2() {
+    const { readFile, writeFile } = await import("fs/promises");
+
+    const fooContent = await readFile("foo.txt", "utf8");
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("done!");
+}
+main2().then(() => {
+    console.log("main2 done!");
+});
