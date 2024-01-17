5ef54b214606edf643dbb568d79ddc3900445612
tetsunari <mtst.tty556@gmail.com>
2024-01-17 22:46:50 +0900

    async await

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import("fs/promises")
-    .then(( { readFile }) =>
-        readFile("package.json", "utf-8")
-    )
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.log("Error: ", error);
-    });
+async function get3(): Promise<number> {
+    console.log('get3() called');
+    return 3;
+}
+console.log('before get3()');
+const p = get3();
+p.then(num => {
+    console.log(`num is ${num}`);
+});
+console.log('after get3()');
+/**
+ * before get3()
+ * get3() called
+ * after get3()
+ * num is 3
+ */
