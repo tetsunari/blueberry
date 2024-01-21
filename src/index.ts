24bf8da99072abbcdac1f78c801ee96b468ee4cf
tetsunari <mtst.tty556@gmail.com>
2024-01-21 18:01:14 +0900

    await エラー処理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { readFile, writeFile } from "fs/promises";
+async function main() {
+    const { readFile, writeFile } = await import("fs/promises");
+    try {
+        const fooContent = await readFile("foo.txt", "utf8");
+        await writeFile("bar.txt", fooContent + fooContent);
+        console.log("Done!");
+    } catch {
+        console.error("Something went wrong!");
+    }
+}
 
-const fooContent = await readFile("foo.txt", "utf8");
-await writeFile("bar.txt", fooContent + fooContent);
-console.log("Done!");
-
-export const bar = fooContent + fooContent;
+main().then(() => {
+    console.log("main() Done!");
+});
