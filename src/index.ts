e3414074ad5ff261c38f3347cae026d5e05f3c9b
tetsunari <mtst.tty556@gmail.com>
2024-01-21 18:19:40 +0900

    async関数のいろいろな宣言2

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
+import { readFile, writeFile } from "fs/promises";
 const main = async function() {
     const fooContent = await readFile("foo.txt", "utf8");
     await writeFile("bar.txt", fooContent + fooContent);
     console.log("success!");
 };
-
+console.log(main());
 // 上と同じ
-const main = async () => {
+const main2 = async () => {
     const fooContent = await readFile("foo.txt", "utf8");
     await writeFile("bar.txt", fooContent + fooContent);
     console.log("success!");
 };
+console.log(main2());
 
 const obj = {
     normalMethod() {
