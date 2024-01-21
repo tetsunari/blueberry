4d1565a7bb6a5bf0878fc61b94f3db0e42337d79
tetsunari <mtst.tty556@gmail.com>
2024-01-21 18:16:04 +0900

    asyncのいろいろな宣言方法

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-async function main() {
-    const { readFile, writeFile } = await import("fs/promises");
-    try {
-        const fooContent = await readFile("foo.txt", "utf8");
-        await writeFile("bar.txt", fooContent + fooContent);
-        console.log("Done!");
-    } catch {
-        console.error("Something went wrong!");
-    }
-}
+const main = async function() {
+    const fooContent = await readFile("foo.txt", "utf8");
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("success!");
+};
 
-main().then(() => {
-    console.log("main() Done!");
-});
+// 上と同じ
+const main = async () => {
+    const fooContent = await readFile("foo.txt", "utf8");
+    await writeFile("bar.txt", fooContent + fooContent);
+    console.log("success!");
+};
+
+const obj = {
+    normalMethod() {
+
+    },
+    async asyncMethod() {
+
+    },
+};
