4e9ed0202aa87c6708ab9c0ba604554c331e3f5f
tetsunari <mtst.tty556@gmail.com>
2024-02-05 23:35:26 +0900

    ユニオン型

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import path from "path";
-import { readFile } from "fs/promises";
-import { fileURLToPath } from "url";
+'use strict';
 
-const errorAfter1ms = async () => {
-    await sleep(1);
-    throw new Error("Time out!");
+{
+  let keyword: string | number | boolean;
+  keyword = 'milk';
+  keyword = 50;
+  keyword = true;
 }
-
-const filePath = fileURLToPath(import.meta.url);
-const fileDir = path.dirname(filePath);
-const dataFile = path.join(fileDir, "../uhyo.txt");
-const data = await Promise.race([
-    readFile(dataFile, { encoding: "utf8"}),
-    errorAfter1ms()
-]).catch(() => {
-    console.log("error");
-    process.exit();
-})
-let count = 0;
-let currentIndex = 0;
-while(true) {
-    const nextIndex = data.indexOf("uhyo", currentIndex);
-    if (nextIndex >= 0) {
-        count++;
-        currentIndex = nextIndex + 1;
-    } else {
-        break;
-    }
-}
-console.log(currentIndex);
