12a72410ddcc3a6b5feaceaa155a025ce81bc217
tetsunari <mtst.tty556@gmail.com>
2024-02-05 23:37:20 +0900

    リテラル型

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 'use strict';
 
 {
-  let keyword: string | number | boolean;
-  keyword = 'milk';
-  keyword = 50;
-  keyword = true;
+  let taxRate: 0.1;
+  let myEmail: 'taro@example.com';
+  let isPaid: true;
+  myEmail = 'jiro@example.com';
+
+  let result: 'Pass' | 'Fail';
+  result = 'Good';
+  result = 50;
 }
