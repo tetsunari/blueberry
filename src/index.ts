80a1166b8c342babbb89f0b33f0a66b118979507
tetsunari <mtst.tty556@gmail.com>
2024-02-05 23:53:12 +0900

    タプル

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 'use strict';
 
 {
-  const scores: readonly number[] = [70, 90, 80];
-  scores[1] = 100;
-  scores.push(60);
-  scores[2] = 'Hello';
-  scores.push('OK');
-
-  const values: (string | number)[] = ['Taro', 70, 'Jiro'];
-  values[2] = 80;
-  values.push('Saburo');
-  values.push(true);
+  const values: readonly [string, number] = ['Taro', 70];
+  values[0] = 'Jiro';
+  values[1] = 90;
+  values[0] = 70;
+  values[1] = 'Saburo';
 }
