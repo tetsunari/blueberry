5f9ad9d499b521d14cd61fb6a607e59db0473328
tetsunari <mtst.tty556@gmail.com>
2024-02-07 00:00:47 +0900

    関数式の型付け

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 'use strict';
 
 {
-  function printUserName(userName: string): void {
-    console.log(userName);
+  // const double = (num: number): number => {
+  //   // return num * 2;
+  //   return 'OK';
+  // };
+
+  let double: (num: number) => number;
+  double = (num: number): number => {
+    // return num * 2;
     return 'OK';
-  }
+  };
+
+  console.log(double(10));
+  console.log(double('OK'));
 }
